de8e70e6e48fda9ff0f7176a56e25aaa254d3514
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-01-19 16:39:15 +0530

    new

--- src/Components/Routes/RoutesComponents.js
+++ src/Components/Routes/RoutesComponents.js
@@ -6,34 +6,14 @@ import Food from './Food'
 import Fitness from './Fitness'
 import Technology from './Technology'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+// import DataComp from '../StoreData/Store'
 import DataComp from '../StoreData/Store'
 import Individual from './Individual'
 
 function RoutesComponents() {
-  // const menustyle={
-  //   textDecoration:'none',
-  //   color: "black",
-  //   fontWeight:"bold",
-  //   fontSize:"large",
-  //   fontFamily:"Serif",
-  //   textShadow:"2px 2px 2px grey"
-  // }
   return (
     <div>
-      {/* <head className='thesirentext'>
-        <h4 className='the'>The</h4> 
-        <h1 className='siren'>Siren</h1>
-      </head> */}
         <BrowserRouter>
-        {/* <nav className='Navbar'>
-        <NavLink to='/' style={menustyle}>   Home   </NavLink>
-        <NavLink to='/Bollywood' style={menustyle} >   Bollywood   </NavLink>
-        <NavLink to='/Hollywood' style={menustyle} >   Hollywood   </NavLink>
-        <NavLink to='/Food' style={menustyle}>   Food   </NavLink>
-        <NavLink to='/Fitness' style={menustyle}>   Fitness   </NavLink>
-        <NavLink to='/Technology' style={menustyle}>   Technology   </NavLink>
-        </nav>
-        <hr style={{width:"100%"}}/> */}
        <DataComp>
         <Routes>
 
